cb2c570e77f5ff40ea2ae712024b0a4888f7a5d1
Descaplis <Kajtas7424@gmail.com>
2024-10-27 00:02:51 +0200

    update

--- client/components/Chat.jsx
+++ client/components/Chat.jsx
@@ -1,22 +1,40 @@
 function Chat() {
     return (
     <>
-        <div id="chatContainer" className="w-1/4 h-full flex flex-col justify-between items-center border border-normalMenu rounded-xl bg-background">
-            <div id="chat" className="h-full flex flex-col justify-start w-11/12 py-3 px-0.5 overflow-y-auto">
+        <div id="chatContainer" className="
+        w-1/4 h-full flex flex-col justify-between items-center 
+        rounded-xl bg-[#d3d3d3] dark:bg-[#404040]">
+            <div id="room" className="w-full flex justify-center items-center">
+                <select id="roomSelect" className="
+                bg-[#a0a1a3] dark:bg-[#aaabad] text-[#181818] text-center text-lg
+                w-[90%] p-2 mt-2 rounded-full
+                hover:cursor-pointer hover:bg-[#8d8f91] dark:hover:bg-[#b6b7ba]">
+                    <option selected>Polska</option>
+                    <option selected>USA</option>
+                    <option selected>Japonia</option>
+                </select>
+            </div>
+            <div id="chat" className="
+            h-full w-11/12 flex flex-col justify-start
+             py-3 px-0.5 overflow-y-auto">
                 <div className="chatMessage w-full flex flex-col justify-center">
                     <div className="chatMessageHeader flex justify-start h-5 gap-2">
-                        <p className="chatMessageUsername font-bold text-[white]">Maciej Sieńko</p>
-                        <p className="chatMessageTime text-[#b9b9b9]">13:49</p>
+                        <p className="chatMessageUsername font-bold text-[#181818] dark:text-[#e6e6e6]">Maciej Sieńko</p>
+                        <p className="chatMessageTime text-[#323232] dark:text-[#b9b9b9]">13:49</p>
                     </div>
-                    <div className="chatMessageContent text-[#e3e3e3]">
+                    <div className="chatMessageContent text-[#181818] dark:text-[#e6e6e6]">
                         <p className="chatMessageText">Polecam hazard</p>
                     </div>
                 </div>
             </div>
-            <div id="chatSendMessageContainer" className="flex justify-center items-center w-full p-3">
-                <input type="text" id="messageInput" className="border border-normalChat w-10/12 rounded-lg mr-0.5 p-1 text-base bg-[#3a3a3a] border-[#cecece]"/>
-                <button type="submit" id="sendMessage" className="w-1/8 h-4/5 p-0.5 border rounded-lg flex justify-center items-center
-                 text-5xl bg-[#ddce77] text-[#252525] border-[#252525] hover:bg-[#bdae5a] hover:cursor-pointer select-none">⊳</button>
+            <div id="chatSendMessageContainer" className="flex justify-center items-center w-full p-1">
+                <input type="text" id="messageInput" className="
+                w-10/12 h-full rounded-lg mr-1 p-1
+                text-base bg-[#a0a1a3] dark:bg-[#5d6066]"/>
+                <button type="submit" id="sendMessage" className="
+                w-[15%] h-full p-0.5 rounded-lg flex justify-center items-center
+                text-4xl text-[#252525] bg-[#dcc43c] hover:bg-[#bdac4f]
+                hover:cursor-pointer select-none">&gt;</button>
             </div>
         </div>
     </>);
--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -24,14 +24,15 @@ function Menu() {
     }
     return (
     <>
-        <div id="menu" className="flex flex-row justify-between w-full h-[12.5vh] border border-black dark:border-white">
+        <div id="menu" className="flex flex-row justify-between 
+        w-full h-[12.5vh] border-b border-black dark:border-white
+        bg-[#d3d3d3] dark:bg-[#404040]">
             <div className="menuSection flex justify-center items-center select-none">
                 <div id="title" className="
-                h-full max-width-2/5 flex justify-center items-center
+                h-full flex justify-center items-center
                  px-2.5 py-0.5 tracking-[0.25rem]
-                 border-x border-[#181818] dark:border-[#f0f0f0]
-                 text-[#c5ba57] dark:text-[#f8eb73]">
-                    <h1 className="text-2xl">KOIN KORNER</h1>
+                 text-[#d0b835] dark:text-[#e6d750]">
+                    <h1 className="text-2xl text-center">KOIN KORNER</h1>
                 </div>
                 {
                     [
@@ -40,9 +41,9 @@ function Menu() {
                         {name: "Dowód uczciwości", href: "/ProvablyFair"}
                     ].map((div, index) => (
                         <div key={index} className="
-                        text-fontColor hover:text-[#505050] dark:hover:text-[#aaaaaa] hover:cursor-pointer
-                        flex justify-center items-center max-w-2/5 h-full 
-                        border-x border-[#181818] dark:border-[#f0f0f0] px-2.5 py-0.5">
+                        text-[#181818] dark:text-[#e6e6e6] text-center hover:text-[#505050] dark:hover:text-[#aaaaaa] hover:cursor-pointer
+                        flex justify-center items-center h-full
+                        px-2.5 py-0.5">
                             <a href={div.href} className="text-xl tracking-widest no-underline">{div.name}</a>
                         </div>
                     ))
@@ -51,20 +52,23 @@ function Menu() {
             
             <div className="menuSection flex justify-center items-center select-none">
                 <div id="theme" className="
-                w-64 h-full max-width-2/5 flex justify-center items-center 
-                border-x border-[#181818] dark:border-[#f0f0f0] px-2.5 py-0.5 hover:cursor-pointer" onClick={changeTheme}>
+                h-5/6 flex justify-center items-center
+                px-2.5 py-0.5 hover:cursor-pointer" onClick={changeTheme}>
                     <Image src={imageSrc} alt="theme icon" draggable={false} className="w-auto h-full invert"/>
                 </div>
                 <div id="coins" className="
                     h-full flex justify-center items-center
                     text-5xl text-[#181818] dark:text-[#f7f7f7] font-title hover:text-[#505050] dark:hover:text-[#aaaaaa]
-                    hover:cursor-pointer border-x border-[#181818] dark:border-[#f0f0f0] px-2.5 py-0.5"
+                    hover:cursor-pointer px-2.5 py-0.5"
                 >
-                    <p className="h-1/2">100</p>
+                    <p className="h-1/2 text-4xl">100</p>
                     <Image src={koinPic} alt="koin" draggable={false} className="h-2/3 w-full ml-2"/>
                 </div>
-                <div id="profile" className="h-full border-0 hover:text-[#aaaaaa] hover:cursor-pointer border-x border-[#f0f0f0] px-2.5 py-0.5">
-                    <Image src={profilePic} alt="profile pic" draggable={false} className="w-full h-full rounded-full"/>
+                <div id="profile" className="
+                h-full hover:text-[#aaaaaa] hover:cursor-pointer
+                px-2.5 py-0.5
+                flex justify-center items-center">
+                    <Image src={profilePic} alt="profile pic" draggable={false} className="w-full h-5/6 rounded-full"/>
                 </div>
             </div>
         </div>

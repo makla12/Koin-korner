35facfd5207be8fab34c9d11138692ec3bf8d10e
makla12 <1234makla@gmail.com>
2024-10-30 10:36:03 +0100

    login page

--- client/components/pages/LoginPage.jsx
+++ client/components/pages/LoginPage.jsx
@@ -3,18 +3,15 @@ import { MainNav } from '@/components/elements/MainNav'
 function LoginPage() {
     return (
     <>
-        <MainNav />
-        <div className="m-4">
-            <a href="./" className="bg-[#dfd478] p-3 text-black m-5 text-xl rounded hover:bg-[#f8eb73]">Powrót</a>
-            <p className="mt-4">Wszystkie role na koin korner są wygenerowane przez system "provably fair". Czyli wynik każdego rolla nie może być manipulowany i jest z góry ustalony. Gracze mogą sprawdzać seed każdego wcześniejszego rolla używając tego kodu:</p>
-            <p className="text-center m-7">
-                $server_seed = "2754f7cc9e9e243711448401ea9d483067138a6b37c70c5d80b6c59943aa6996";<br/>
-                $lotto = "1135567382";<br/>
-                $round_id = "1";<br/>
-                $hash = hash("sha256", $server_seed . "-" . $lotto . "-" . $round_id);<br/>
-                $roll = hexdec(substr($hash, 0, 8)) % 15;<br/>
-                echo "Round $round_id = $roll";<br/>
-            </p>
+
+        <div className="flex justify-center items-center m-14">
+            <form className="
+                w-[30vw] h-[66vh] bg-[#d3d3d3] dark:bg-[#27272a] text-[#303030] dark:text-[#f3f3f3]
+                rounded-lg p-4 flex justify-center items-start"
+            >
+                <label htmlFor="username" className="text-lg">Nazwa użytkownika:</label>
+                <input type="text" id="username" className="text-md text-[#303030]"/>
+            </form>
         </div>
     </>
     );

dbea4c80427aa4272f4077eae1eacd4c8d975d9e
Descaplis <Kajtas7424@gmail.com>
2024-10-30 10:22:41 +0100

    update

--- client/components/elements/GameContainer.jsx
+++ client/components/elements/GameContainer.jsx
@@ -5,7 +5,7 @@ function GameContainer({ children }) {
         <div className="w-full h-full ml-5 rounded-lg flex flex-col justify-start items-center">
             <GamesNav />
             
-            <div className="w-full min-h-[85%] mt-5 rounded-2xl bg-[#d3d3d3] dark:bg-[#404040]">
+            <div className="w-full min-h-[85%] mt-5 rounded-2xl bg-[#d3d3d3] dark:bg-[#27272a]">
                 {children}
             </div>
         </div>
--- client/components/elements/GamesNav.jsx
+++ client/components/elements/GamesNav.jsx
@@ -1,8 +1,8 @@
 function GamesNav() {
     return (
     <div className="
-        w-full h-[12.5%] flex justify-center items-center 
-        bg-[#d3d3d3] dark:bg-[#404040] rounded-full"
+        w-full h-[12.5%] flex justify-center items-center  
+        bg-[#d3d3d3] dark:bg-[#27272a] rounded-full"
     >
         {[
             {name: "Ruletka", href: "/roulette"},
@@ -12,8 +12,8 @@ function GamesNav() {
         ]
         .map((div, index) => (
             <a key={index} href={div.href} className="
-                text-[#181818] text-2xl w-1/5 h-4/5  flex justify-center items-center 
-                bg-[#cfb93d] hover:cursor-pointer hover:bg-[#e0cb58] select-none
+                text-[#181818] text-2xl w-1/5 h-[70%]  flex justify-center items-center 
+                bg-yellow-500 hover:cursor-pointer hover:bg-yellow-400 select-none
                 p-1.5 rounded-full mx-2"
             >
                 <h2>{div.name}</h2>

f22c7f6bc43277e00f7d8474dd8690a1dd5863d4
makla12 <144844324+makla12@users.noreply.github.com>
2024-11-05 19:21:24 +0100

    Update ProvablyFairPage.jsx

--- client/components/pages/ProvablyFairPage.jsx
+++ client/components/pages/ProvablyFairPage.jsx
@@ -1,23 +1,20 @@
-
+import { MainNav } from "@/components/elements/MainNav";
 function ProvablyFairPage() {
   	return (
+	<>
+		<MainNav />
     	<div className="m-4">
-      		<a href="./" className="
-                text-[#181818] text-2xl w-48 flex justify-center items-center
-                bg-yellow-400 hover:cursor-pointer hover:bg-yellow-300 select-none
-                p-1 rounded-full
-                "
-                >Powrót</a>
-        		<p className="mt-4">Wszystkie role na koin korner są wygenerowane przez system "provably fair". Czyli wynik każdego rolla nie może być manipulowany i jest z góry ustalony. Gracze mogą sprawdzać seed każdego wcześniejszego rolla używając tego kodu:</p>
-        		<p className="text-center m-7">
-            	$server_seed = "2754f7cc9e9e243711448401ea9d483067138a6b37c70c5d80b6c59943aa6996";<br/>
-				$lotto = "1135567382";<br/>
-				$round_id = "1";<br/>
-				$hash = hash("sha256", $server_seed . "-" . $lotto . "-" . $round_id);<br/>
-				$roll = hexdec(substr($hash, 0, 8)) % 15;<br/>
-				echo "Round $round_id = $roll";<br/>
-        		</p>
+			<p className="mt-4">Wszystkie role na koin korner są wygenerowane przez system "provably fair". Czyli wynik każdego rolla nie może być manipulowany i jest z góry ustalony. Gracze mogą sprawdzać seed każdego wcześniejszego rolla używając tego kodu:</p>
+			<p className="text-center m-7">
+			$server_seed = "2754f7cc9e9e243711448401ea9d483067138a6b37c70c5d80b6c59943aa6996";<br/>
+			$lotto = "1135567382";<br/>
+			$round_id = "1";<br/>
+			$hash = hash("sha256", $server_seed . "-" . $lotto . "-" . $round_id);<br/>
+			$roll = hexdec(substr($hash, 0, 8)) % 15;<br/>
+			echo "Round $round_id = $roll";<br/>
+			</p>
     	</div>
+	</>
   	);
 }
 

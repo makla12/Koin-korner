862a237449040458447a0c19524b83baaddc0784
makla12 <1234makla@gmail.com>
2024-12-16 12:18:35 +0100

    Update RouletteBet.jsx

--- client/components/elements/RouletteBet.jsx
+++ client/components/elements/RouletteBet.jsx
@@ -7,7 +7,7 @@ function RouletteBet({name, bet, choice, type, self}){
             <div className="flex items-center gap-1">
                 <p className="font-bold text-lg">{bet}</p>
                 <div className={`min-w-4 w-auto h-8 border-2 ${(type == 0 ? "bg-red-600 border-red-800" : (type == 1 ? "bg-yellow-600 border-yellow-800" : "bg-gray-900 border-gray-950"))} flex justify-center items-center rounded-lg`}>
-                    {(choice == "RED" || choice == "BLACK" ? "" : (choice == "EVEN" ? "E" : (choice == "ODD" ? "O" : choice)))}
+                    {(choice == "RED" || choice == "BLACK" ? "" : (choice == "EVEN" ? "P" : (choice == "ODD" ? "N" : choice)))}
                 </div>
             </div>
         </div>
